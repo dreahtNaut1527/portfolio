3e6f510d01c62de59ce379692247a9c2c57e7cfb
tubs <tubs33959@gmail.com>
2022-02-18 13:56:56 +0800

    Created New Portfolio

---
+++ commit - portfolio.bat
@@ -0,0 +1,29 @@
+set https_proxy=http://hrdproxy.hrd-s.com:81
+cd C:\Users\smd153\Desktop\Task\03.Web Development\apps\portfolio
+git init
+git remote rm origin
+git remote add origin https://github.com/dreahtNaut1527/portfolio.git
+git add .
+git commit -m "Created New Portfolio"
+git push -u https://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@github.com/dreahtNaut1527/portfolio.git
+
+
+pause
+
+token: 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
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
